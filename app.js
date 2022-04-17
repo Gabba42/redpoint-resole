9d8d14d9c91918e8d89ee307bb54d5678d417641
Gabriela Lisboa <GLisboa@Gabrielas-MacBook-Air-4.local>
2022-04-16 20:45:25 -0600

    edit app.js file so morgan('dev') is above express; need to do that in order to see calls/status

--- app.js
+++ app.js
@@ -2,19 +2,23 @@
 
 const express = require('express');
 const morgan = require('morgan');
-const pg = require('pg');
+const { Postgres } = require('pg');
 const bodyParser = require('body-parser');
 
 const app = express(); 
-const port = process.env.PORT || 8080; //Heroku is going to use the env.PORT
+const PORT = process.env.PORT || 8080; //Heroku is going to use the env.PORT
 
 app 
+    .use(morgan('dev')) //saves everything in dev mode; put morgan first or will not be able to see http request details in console
     .use(express.static('public')) //will serve any static files in the 'public' folder
-    .use(morgan('dev')) //saves everything in dev mode
 
     .use(bodyParser.urlencoded({ extended: false }))
     .use(bodyParser.json())
 
-    .listen(port, () => console.log(`Server listening on port ${port}`));
+    .post('/api/resoledata', (req, res, next) => {
+        res.json(req.body);
+    })
+
+    .listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
     
